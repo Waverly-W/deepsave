21a83fb8dda190f2a1719e1fa3f7812903fee2ef
Waverly <wf1095870395@gmail.com>
2026-02-07 22:15:31 +0800

    delete

--- backend/app/api/v1/api.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-from app.api.v1.endpoints import auth, items
-
-api_router = APIRouter()
-api_router.include_router(auth.router, tags=["login"])
-api_router.include_router(items.router, prefix="/items", tags=["items"])
-# api_router.include_router(users.router, prefix="/users", tags=["users"])
